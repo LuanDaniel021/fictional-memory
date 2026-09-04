4189a8f3a0467e54a2493cb821da4532ce68d7b2
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 16:28:20 -0300

    update create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'José Silva' })
+  nome: string;
+
+  @IsEmail()
+  @ApiProperty({ example: 'usuario@email.com' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @ApiProperty({ example: '123456' })
+  password: string;
+}
