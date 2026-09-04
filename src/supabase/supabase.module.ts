6f5194c72bb9e5554636f2e87357a1152a258d1e
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 17:17:00 -0300

    fix: jwt-key adicionada ao supabase.strategy (provisorio), user.mudules corrigido, class-transformer instalado

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
 import { ConfigModule } from '@nestjs/config';
 
-Module({
+@Module({
   imports: [ConfigModule],
   providers: [SupabaseService],
   exports: [SupabaseService],
