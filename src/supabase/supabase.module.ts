589ef18e9060a92191d04904fa0b357f838859b4
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 15:06:49 -0300

    integrando supabase ao recurso

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SupabaseService } from './supabase.service';
-import { SupabaseController } from './supabase.controller';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({
-  controllers: [SupabaseController],
+Module({
+  imports: [ConfigModule],
   providers: [SupabaseService],
+  exports: [SupabaseService],
 })
 export class SupabaseModule {}
--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
-export class SupabaseService {}
+export class SupabaseService {
+
+  private client: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    const url = this.configService.get<string>('SUPABASE_URL');
+    const key = this.configService.get<string>('SUPABASE_SECRET_KEY');
+
+    if (!url || !key) {
+      throw new Error(
+        'SUPABASE_URL e SUPABASE_SECRET_KEY precisam ser definidos no .env',
+      );
+    }
+
+    this.client = createClient(url, key);
+  }
+
+  getClient(): SupabaseClient {
+    return this.client;
+  }
+}
