3a4c2f850a1348e9c1d3281e39dfa3c25c65d376
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 15:26:46 -0300

    add '--save-dev @types/passport-jwt' e inicio de SupabaseStrategy

--- src/supabase/supabase.strategy.ts
+++ src/supabase/supabase.strategy.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
 
 @Injectable()
-export class SupabaseStrategy {}
+export class SupabaseStrategy extends PassportStrategy(Strategy, 'supabase') {
+    validate(...args: any[]): unknown {
+        throw new Error('Method not implemented.');
+    }
+}
