715e390e6f42570916e8a1337dc288531dd62157
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 16:59:04 -0300

    1 - fix: JWT error

--- src/supabase/supabase.strategy.ts
+++ src/supabase/supabase.strategy.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class SupabaseStrategy extends PassportStrategy(Strategy, 'supabase') {
+    constructor(private readonly config: ConfigService) {
+        const url = config.get<string>('SUPABASE_URL');
+        const key = config.get<string>('SUPABASE_SECRET_KEY');
+
+        if (!url || !key) {
+            throw Error(
+                'SUPABASE_URL ou SUPABASE_SECRET_KEY, não definido'
+            );
+        }
+
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey:key,
+            algorithms: ['RS256'],
+        });
+    }
     validate(...args: any[]): unknown {
         throw new Error('Method not implemented.');
     }
