037becc8d2a561faf6594f2767af7f1ce77148c7
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-04 16:12:00 -0300

    instalando swagger e configurando main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Fictional Memory API')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Insira o JWT de acesso do Supabase',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
