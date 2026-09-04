bbc80c2cd36bb386bd331a1386043b3c789baecd
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-03 21:41:32 -0300

    dependecias '@supabase/supabase-js' e '@nestjs/config' instaladas, recursos 'users' e 'supabase' criados com nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, SupabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
