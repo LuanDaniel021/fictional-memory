fd8209a2123e63d55a727a358b2021510bb37f57
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-09 16:38:28 -0300

    init resource dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { UsersModule } from './users/users.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
-    SupabaseModule
+    SupabaseModule,
+    DashboardModule
   ],
   controllers: [AppController],
   providers: [AppService],
