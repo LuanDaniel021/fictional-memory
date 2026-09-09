8cfdff18c3bbfce1dc14fdba2f2fb5e27ad454d8
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-09 16:42:11 -0300

    exclude prefix from main route

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: '/', method: RequestMethod.GET }
+    ]
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
