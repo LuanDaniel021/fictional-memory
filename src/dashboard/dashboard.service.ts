2e4ffce2ad781c78dac7d59d6cf4448366c2cacb
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-09 16:44:09 -0300

    remove methods from service and controller in dashboard

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -4,23 +4,7 @@ import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 
 @Injectable()
 export class DashboardService {
-  create(createDashboardDto: CreateDashboardDto) {
-    return 'This action adds a new dashboard';
-  }
-
-  findAll() {
+  async findAll() {
     return `This action returns all dashboard`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} dashboard`;
-  }
-
-  update(id: number, updateDashboardDto: UpdateDashboardDto) {
-    return `This action updates a #${id} dashboard`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} dashboard`;
   }
 }
