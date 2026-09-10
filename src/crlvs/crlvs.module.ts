90016a02465ee4b8cb85dc3c71b7569df7b96d84
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-10 02:02:33 -0300

    ajustas em contollers

--- src/crlvs/crlvs.module.ts
+++ src/crlvs/crlvs.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CrlvService } from './crlvs.service';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { CrlvController } from './crlvs.controller';
 
 @Module({
   imports: [ SupabaseModule ],
+  controllers: [CrlvController],
   providers: [CrlvService],
   exports: [CrlvService]
 })
--- src/pneus/pneus.module.ts
+++ src/pneus/pneus.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PneusService } from './pneus.service';
-// import { PneusController } from './pneus.controller';
+import { PneusController } from './pneus.controller';
 
 @Module({
+  controllers: [PneusController],
   providers: [PneusService],
   exports: [PneusService]
 })
