9cbe9d108bee8aa781aa02fb5295d2e95be1a168
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-10 01:03:52 -0300

    exports MotoristasService

--- src/motoristas/motoristas.module.ts
+++ src/motoristas/motoristas.module.ts
@@ -1,3 +1,4 @@
+
 import { Module } from '@nestjs/common';
 import { MotoristasService } from './motoristas.service';
 import { MotoristasController } from './motoristas.controller';
@@ -5,5 +6,6 @@ import { MotoristasController } from './motoristas.controller';
 @Module({
   controllers: [MotoristasController],
   providers: [MotoristasService],
+  exports: [MotoristasService]
 })
 export class MotoristasModule {}
