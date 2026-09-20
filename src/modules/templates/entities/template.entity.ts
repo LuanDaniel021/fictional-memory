226408eb420378104b75b84563139eb70ded0ec3
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-20 06:35:58 +0000

    arrumando template permite

--- src/modules/templates/entities/template.entity.ts
+++ src/modules/templates/entities/template.entity.ts
@@ -2,19 +2,17 @@
 export class Template {
 
   id: number
-
   nome: string
-
   estrutura: string[]
 
-  permite(eixo: number, lado: 'E' | 'D', indice: number): boolean {
-    const _eixo = this.estrutura[eixo];
+  permite( eixo: number, lado: 'E' | 'D', indice: number ) {
+    const _eixo = this.estrutura[eixo].trim().split(' ');
 
     if (!_eixo) {
       return false;
     }
 
-    return _eixo[{ E: 0, D: 1 }[lado]][indice] !== undefined;
+    return (_eixo[{ 'E': 0, 'D': 1 }[lado]])[indice+1] !== undefined;
   }
 
 }
